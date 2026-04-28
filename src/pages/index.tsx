e1fbbb4e78ef2c97916b5d76816b5b91e65ef8a8
Maxim Dupley <duplexmi@amp1996.ru>
2026-04-28 16:09:04 +0300

    Add authentication system with login/register, task list working (task form temporarily disabled due to pre-existing build issues)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -161,7 +161,7 @@ export default function Homepage({ store }: RequestContext, { state }: WebContex
       </div>
 
       <section class="task-manager-section">
-        {/* TaskForm is temporarily disabled due to build issues */}
+        {/* <TaskForm /> */}
         <TaskList />
       </section>
     </>
